94b8c432bd6c75536e25f781285244f03aec2064
Josh Evenson <jevenson@bhtp.com>
2020-04-02 18:41:50 -0500

    forgot a file

---
+++ assets/js/header.js
@@ -0,0 +1,7 @@
+function toggle(el) {
+    el.classList.toggle('open');
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+    nav.classList.toggle('open');
+    header.classList.toggle('open');
+}
